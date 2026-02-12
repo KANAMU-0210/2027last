005c83edc822737e4a7a9d31e2dac669291c4aba
KANAMU-0210 <sskoumi@icloud.com>
2026-02-12 15:47:34 +0900

    え
    ええええ

---
+++ LastTest1/LastTest1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MemoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Save = new System.Windows.Forms.Button();
+            this.ShowFile = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(101, 91);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(105, 43);
+            this.Save.TabIndex = 0;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // ShowFile
+            // 
+            this.ShowFile.Location = new System.Drawing.Point(101, 176);
+            this.ShowFile.Name = "ShowFile";
+            this.ShowFile.Size = new System.Drawing.Size(105, 43);
+            this.ShowFile.TabIndex = 1;
+            this.ShowFile.Text = "ShowFile";
+            this.ShowFile.UseVisualStyleBackColor = true;
+            this.ShowFile.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(101, 256);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(105, 43);
+            this.Delete.TabIndex = 2;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(263, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 352);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 19);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.ShowFile);
+            this.Controls.Add(this.Save);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button ShowFile;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
